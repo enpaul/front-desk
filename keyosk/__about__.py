0516970935575c4d829276319a5856b21ecad60e
Ethan Paul <e@enp.one>
2020-02-18 22:58:33 -0500

    Update about module to pull from pyproject

--- keyosk/__about__.py
+++ keyosk/__about__.py
@@ -1,8 +1,12 @@
-# pylint: disable=missing-docstring
-__title__ = "front-desk"
-__version__ = "0.1.0"
-__summary__ = "Web application for managing JSON Web Tokens"
-__author__ = "Ethan Paul"
-__author_email__ = "e@enp.one"
-__url__ = "http://github.com/enpaul/front-desk"
-__license__ = "MIT"
+"""Access package info programatically without duplication"""
+import importlib.resources
+
+import toml
+
+PYPROJECT = toml.loads(importlib.resources.read_text("keyosk", "pyproject.toml"))
+
+__authors__ = PYPROJECT["tool"]["poetry"]["authors"]
+__summary__ = PYPROJECT["tool"]["poetry"]["description"]
+__title__ = PYPROJECT["tool"]["poetry"]["name"]
+__url__ = PYPROJECT["tool"]["poetry"]["repository"]
+__version__ = PYPROJECT["tool"]["poetry"]["version"]
