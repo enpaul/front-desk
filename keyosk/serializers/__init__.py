b7596bdd3fbeb991cbbe89b3b13d337538d65005
Ethan Paul <e@enp.one>
2020-02-24 21:29:38 -0500

    Add serializer submodule imports

---
+++ keyosk/serializers/__init__.py
@@ -0,0 +1,6 @@
+# pylint: disable=missing-docstring
+from keyosk.serializers.account import AccountSerializer
+from keyosk.serializers.account_acl import AccountACLSerializer
+from keyosk.serializers.domain import DomainSerializer
+from keyosk.serializers.domain_extras import DomainAccessListSerializer
+from keyosk.serializers.domain_extras import DomainPermissionSerializer
